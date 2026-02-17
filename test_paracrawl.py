3f623cbe4039822478b94db21ce9c8cd3b37f2d6
diixo <anatolii.podoba@gmail.com>
2026-02-17 06:05:56 +0200

    test_paracrawl.py

--- test_paracrawl.py
+++ test_paracrawl.py
@@ -2,7 +2,16 @@
 from itertools import islice
 
 
+def str_tokenize_words(s: str):
+    import re
+    s = re.findall("(\.?\w[\w'\.&-]*\w|\w\+*#?)", s)
+    if s: return s
+    return []
+
+
 count = 50
+
+words = set()
 
 
 if __name__ == "__main__":
@@ -10,15 +19,28 @@ if __name__ == "__main__":
     #with open("datasets/paracrawl-eu/en-ru.txt", "r", encoding="utf-8") as f:
     with open("data/manythings.org/rus.txt", "r", encoding="utf-8") as f:
 
-        for i, line in enumerate(islice(f, count), 1):
 
+        i = 0
+        for line in f:
             n_tabs = line.count("\t")
 
             if n_tabs == 0:
                 continue
 
+            i += 1
+
+            if i > count:
+                break
+
             # always get two first columns, even if there are more
-            first, second = line.split("\t", 2)[:2]
+            eng, translation = line.split("\t", 2)[:2]
 
-            print(i, first[:80], second[:80])
+            print(i, eng[:80], translation[:80])
 
+            ws = str_tokenize_words(eng.lower())
+            words.update(ws)
+
+print(40*"#")
+print("unique words:", len(sorted(words)))
+
+file_name = "html/STAY _ English meaning - Cambridge Dictionary.html"
