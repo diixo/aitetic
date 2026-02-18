56a688ca743803ce360e7ccf2171a16c6451e072
diixo <anatolii.podoba@gmail.com>
2026-02-18 08:14:02 +0200

    test_slotting_constructor.py: initial commit

---
+++ test_slotting_constructor.py
@@ -0,0 +1,34 @@
+
+
+Person_names = [
+    'Aaron', 'Adam', 'Alan', 'Alexander', 'Alfred', 'Alice', 'Amanda', 'Amber', 'Amelia', 'Amy', 'Anatole', 'Anna', 'Audrey',
+    'Anastasia', 'Andrew', 'Angela', 'Angelina', 'Anna', 'Anthony', 'Arthur', 'Ashley',
+    'Barbara', 'Benjamin', 'Betty', 'Bill', 'Bob', 'Brad', 'Brandon', 'Brenda', 'Brian', 'Bruce', 'Bryan',
+    'Cameron', 'Carl', 'Carlos', 'Carol', 'Caroline', 'Carolyn', 'Casey', 'Catherine', 'Cecil', 'Cedric', 'Charles', 'Charlie',
+    'Charlotte', 'Cheryl', 'Christian', 'Christine', 'Christopher', 'Christy', 'Cindy', 'Clara', 'Crystal', 'Cynthia',
+    'Daniel', 'David', 'Deborah', 'Dennis', 'Diana', 'Donald', 'Donna', 'Dorothy', "Dylan",
+    'Edward', 'Eleanor', 'Elizabeth', 'Emily', 'Emma', 'Eric', 'Ethan', 'Eve', 'Evelyn',
+    'Frank', 'Frederick',
+    'Gabriel', 'Gabriella', 'Gary', 'George', 'Gerald', 'Grace', 'Gregory', "Greg",
+    'Hannah', 'Halle', 'Harold', 'Helen', 'Henry', 'Howard', 'Hugh',
+    'Ian', 'Isaac', 'Isabella', 'Ivan', 
+    'Jack', 'Jacob', 'Jake', 'James', 'Jamie', 'Jane', 'Jason', 'Jasper', 'Jay', 'Jeff', 'Jeffrey',
+    'Jennifer', 'Jenny', 'Jeremy', 'Jerome', 'Jerry', 'Jesse', 'Jessica', 'Jim', 'Jimmy', 'Joe', 'Joel',
+    'John', 'Johnny', 'Jonathan', 'Jordan', 'Jose', 'Joseph', 'Josh', 'Joshua', 'Julia', 'Justin',
+    'Karen', 'Katherine', 'Kathleen', 'Katie', 'Keith', 'Kenneth', 'Kevin', 'Kimberly', 'Kyle',
+    'Larry', 'Laura', 'Linda', 'Lisa', 'Liz',
+    'Margaret', 'Mark', 'Martin', 'Mary', 'Matthew', 'Megan', 'Melanie', 'Melissa', 'Michael', 'Michelle', "Mike", 'Molly', 'Monica', 'Morgan',
+    'Nancy', 'Natalie', 'Nathan', 'Nicholas', 'Nicole',
+    'Olivia', 'Oscar', 
+    'Pamela', 'Patrick', 'Paul', 'Peter', 'Philip',
+    'Rachel', 'Raymond', 'Rebecca', 'Robert', 'Roger', 'Ronald', 'Ryan', "Ronny", "Ronnie",
+    'Samantha', 'Samuel', 'Sandra', 'Sarah', 'Scarlett', 'Scott', 'Sharon', 'Shirley', 'Sophia', 'Stephanie', 'Stephen', 'Steven', 'Susan',
+    'Teddy', 'Terry', 'Theodore', 'Thomas', 'Tim', 'Timothy', 'Tina', 'Tom', 'Tyler',
+    'Vanessa', 'Veronica', 'Victor', 'Victoria', 'Vincent',
+    'Walter', "Warren", 'William',
+    'Zachary']
+
+
+if __name__ == "__main__":
+
+    print("Total unique person names:", len(Person_names))
