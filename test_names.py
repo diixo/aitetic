83a557724c4db3a7228ace271aa95929e1843ae1
diixo <anatolii.podoba@gmail.com>
2026-02-19 18:14:51 +0200

    test_names.py

--- test_names.py
+++ test_names.py
@@ -1,12 +1,12 @@
 
 
 Person_names = [
-    'Aaron', 'Adam', 'Alan', 'Alexander', 'Alfred', 'Alice', 'Amanda', 'Amber', 'Amelia', 'Amy', 'Anatole', 'Anna', 'Audrey',
+    'Aaron', 'Adam', 'Alan', "Alex", 'Alexander', 'Alfred', 'Alice', "Allegra", 'Amanda', 'Amber', 'Amelia', 'Amy', 'Anatole', 'Anna', 'Audrey',
     'Anastasia', 'Andrew', 'Angela', 'Angelina', 'Anna', 'Anthony', 'Arthur', 'Ashley',
     'Barbara', 'Benjamin', 'Betty', 'Bill', 'Bob', 'Brad', 'Brandon', 'Brenda', 'Brian', 'Bruce', 'Bryan',
     'Cameron', 'Carl', 'Carlos', 'Carol', 'Caroline', 'Carolyn', 'Casey', 'Catherine', 'Cecil', 'Cedric', 'Charles', 'Charlie',
-    'Charlotte', 'Cheryl', 'Christian', 'Christine', 'Christopher', 'Christy', 'Cindy', 'Clara', 'Crystal', 'Cynthia',
-    'Daniel', 'David', 'Deborah', 'Dennis', 'Diana', 'Donald', 'Donna', 'Dorothy', "Dylan",
+    'Charlotte', 'Cheryl', "Chris", 'Christian', 'Christine', 'Christopher', 'Christy', 'Cindy', 'Clara', 'Crystal', 'Cynthia',
+    'Daniel', 'David', 'Deborah', 'Dennis', 'Diana', 'Diane', 'Donald', 'Donna', 'Dorothy', "Dylan",
     'Edward', 'Eleanor', "Elvis", 'Elizabeth', 'Emily', 'Emma', 'Eric', 'Ethan', 'Eve', 'Evelyn',
     'Frank', 'Frederick',
     'Gabriel', 'Gabriella', 'Gary', 'George', 'Gerald', 'Grace', 'Gregory', "Greg",
@@ -15,15 +15,15 @@ Person_names = [
     'Jack', 'Jacob', 'Jake', 'James', 'Jamie', 'Jane', 'Jason', 'Jasper', 'Jay', 'Jeff', 'Jeffrey',
     'Jennifer', 'Jenny', 'Jeremy', 'Jerome', 'Jerry', 'Jesse', 'Jessica', 'Jim', 'Jimmy', 'Joe', 'Joel',
     'John', 'Johnny', 'Jonathan', 'Jordan', 'Jose', 'Joseph', 'Josh', 'Joshua', 'Julia', 'Justin',
-    'Karen', 'Katherine', 'Kathleen', 'Katie', 'Keith', 'Kenneth', 'Kevin', 'Kimberly', 'Kyle',
+    'Karen', "Kate", 'Katherine', 'Kathleen', 'Katie', 'Keith', 'Kenneth', 'Kevin', 'Kimberly', 'Kyle',
     'Larry', 'Laura', "Lewis", 'Linda', 'Lisa', 'Liz',
     'Margaret', 'Mark', 'Martin', 'Mary', 'Matthew', 'Megan', 'Melanie', 'Melissa', 'Michael', 'Michelle', "Mike", 'Molly', 'Monica', 'Morgan',
     'Nancy', 'Natalie', 'Nathan', 'Nicholas', 'Nicole',
-    'Olivia', 'Oscar', 
+    'Olivia', 'Oscar',
     'Pamela', 'Patrick', 'Paul', 'Peter', 'Philip', "Phil", "Priscilla",
-    'Rachel', "Randy", "Ralph", 'Raymond', 'Rebecca', 'Robert', 'Roger', 'Ronald', 'Ryan', "Ronny", "Ronnie", "Robin",
-    'Samantha', 'Samuel', 'Sandra', 'Sarah', 'Scarlett', 'Scott', 'Sharon', 'Shirley', 'Sophia', 'Stephanie', 'Stephen', 'Steven', 'Susan', "Simon", "Simona",
-    'Teddy', 'Terry', 'Theodore', 'Thomas', 'Tim', 'Timothy', 'Tina', 'Tom', 'Tyler', "Tony",
+    'Rachel', "Randy", "Ralph", 'Raymond', 'Rebecca', "Richard", 'Robert', 'Roger', 'Ronald', 'Ryan', "Ronny", "Ronnie", "Robin",
+    "Sabrina", 'Samantha', 'Samuel', 'Sandra', 'Sarah', 'Scarlett', 'Scott', 'Sharon', 'Shirley', 'Sophia', 'Stephanie', 'Stephen', 'Steven', 'Susan', "Simon", "Simona",
+    'Teddy', 'Terry', "Terence", 'Theodore', 'Thomas', 'Tim', 'Timothy', 'Tina', 'Tom', 'Tyler', "Tony",
     'Vanessa', 'Veronica', 'Victor', 'Victoria', 'Vincent',
     'Walter', "Warren", 'William', "Winston", "Wesley", "Woodrow"
     ]
